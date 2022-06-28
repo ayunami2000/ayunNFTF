05fec3bc621641ffdca8fdf6b39a949972b500f0
ayunami2000 <spwilliamsiam@gmail.com>
2022-06-28 18:54:19 -0400

    become paranoid

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fardserver = 'freedom.play.totalfreedom.me';
 const chatDelay = 1500;
 const nftGenDelay = 1000 * 60 * 60 * 2; // every 2 hours
 const mapartRate = 4;
-const antiAfkTimer = 1000 * 60 * 13; // every 13 minutes
+const antiAfkTimer = 1000 * 60 * 5; // every 5 minutes
 
 let nftCounter = 0;
 
